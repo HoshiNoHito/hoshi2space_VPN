90518e7f72e5ad2a2f84d987cfb3bd3658bad5be
Saddy0boY <57831787+Saddy0boY@users.noreply.github.com>
2026-07-31 12:57:28 +0300

    Update public.py

--- app/routers/public.py
+++ app/routers/public.py
@@ -8,3 +8,18 @@ templates = Jinja2Templates(directory="app/templates")
 @router.get("/")
 def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+@router.get("/news")
+def news(request: Request):
+    return templates.TemplateResponse("news.html", {"request": request})
+
+
+@router.get("/downloads")
+def downloads(request: Request):
+    return templates.TemplateResponse("downloads.html", {"request": request})
+
+
+@router.get("/faq")
+def faq(request: Request):
+    return templates.TemplateResponse("faq.html", {"request": request})
