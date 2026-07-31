74cc77670144e1de6ff46a8d01784702dd0b7a82
Saddy0boY <57831787+Saddy0boY@users.noreply.github.com>
2026-07-31 12:23:24 +0300

    Update seed_plans.py
    Updated pricing and inbound_ids for plans.

--- seed_plans.py
+++ seed_plans.py
@@ -14,9 +14,9 @@ Base.metadata.create_all(bind=engine)
 db = SessionLocal()
 
 plans = [
-    {"name": "Lite", "price_rub": 150, "inbound_ids": [1]},
-    {"name": "Standart", "price_rub": 250, "inbound_ids": [1, 4]},
-    {"name": "MAX", "price_rub": 400, "inbound_ids": [1, 4},
+    {"name": "Lite", "price_rub": 200, "inbound_ids": [1]},
+    {"name": "Standart", "price_rub": 350, "inbound_ids": [1, 4]},
+    {"name": "MAX", "price_rub": 500, "inbound_ids": [1, 4},
 ]
 
 for p in plans:
